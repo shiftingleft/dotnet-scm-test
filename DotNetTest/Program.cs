6a87fd3841612f2a59dc17dd3093e626c227f316
James Newman <99325304+jnewman-sonatype@users.noreply.github.com>
2022-04-07 10:50:51 +0100

    Source files added.

---
+++ DotNetTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DotNetTest Exiting Normally.");
+        }
+    }
+}
